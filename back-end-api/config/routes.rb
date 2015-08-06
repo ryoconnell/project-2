a0b1a4209c8cb2231b2e2313a6749680b648684d
ryoconnell <ry.oconnell@gmail.com>
2015-08-06 09:12:04 -0400

    First commit since restating api on Thursday.

--- back-end-api/config/routes.rb
+++ back-end-api/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :pets, except: [:new, :edit]
+  resources :locations, except: [:new, :edit]
+  resources :users, except: [:new, :edit]
   resources :contacts, except: [:new, :edit]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
